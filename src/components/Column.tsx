c646814cd2e5cec6bc224916142b544af9f337a7
faun3 <antonioculcea@gmail.com>
2023-10-01 14:24:10 +0300

    feat: add parallax scroll

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,18 +1,30 @@
+import { motion } from "framer-motion";
+import { MotionValue } from "framer-motion";
+
 type TColumnProps = {
   images: string[];
+  y: MotionValue<number>;
+  className?: string;
 };
 
-const Column = ({ images }: TColumnProps) => {
+const Column = ({ images, y, className }: TColumnProps) => {
   return (
-    <>
+    <motion.div
+      className={`flex flex-col w-[25%] gap-[2rem] ${className} relative`}
+      style={{ y }}
+    >
       {images.map((image) => {
         return (
-          <div>
-            <img src={`/gallery/${image}`} alt={image} />
+          <div className="overflow-hidden rounded-[10px]">
+            <img
+              src={`/gallery/${image}`}
+              alt={image}
+              className="object-cover w-full h-full"
+            />
           </div>
         );
       })}
-    </>
+    </motion.div>
   );
 };
 export default Column;
--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,4 +1,6 @@
 import Column from "./Column.tsx";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
 
 const images = [
   "1.jpg",
@@ -16,12 +18,41 @@ const images = [
 ];
 
 const Gallery = () => {
+  const container = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: container,
+    offset: ["start end", "end start"],
+  });
+  const y = useTransform(
+    scrollYProgress,
+    [0, 1],
+    [0, window.innerHeight * 2.85]
+  );
+  const y2 = useTransform(
+    scrollYProgress,
+    [0, 1],
+    [0, window.innerHeight * 3.45]
+  );
+  const y3 = useTransform(
+    scrollYProgress,
+    [0, 1],
+    [0, window.innerHeight * 1.45]
+  );
+  const y4 = useTransform(scrollYProgress, [0, 1], [0, window.innerHeight * 3]);
   return (
-    <>
-      <Column images={images.slice(0, 5)} />
-      <Column images={images.slice(5, 9)} />
-      <Column images={images.slice(9, 12)} />
-    </>
+    <main className="bg-stone-600">
+      <div className="h-screen"></div>
+      <div
+        className="flex flex-row gap-[2rem] p-[4rem] h-[175vh] overflow-hidden bg-black"
+        ref={container}
+      >
+        <Column images={images.slice(0, 3)} y={y} className="top-[-100%]" />
+        <Column images={images.slice(3, 6)} y={y2} className="top-[-95%]" />
+        <Column images={images.slice(6, 9)} y={y3} className="top-[-40%]" />
+        <Column images={images.slice(9, 12)} y={y4} className="top-[-95%]" />
+      </div>
+      <div className="h-screen"></div>
+    </main>
   );
 };
 export default Gallery;
